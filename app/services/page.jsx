9b28cc3ef63cee05c700bf03f8f260826de3e149
webexguruhasan <webexguruhasan95@gmail.com>
2024-06-12 22:22:57 +0600

    [COMPLETED]: Service page design and functionality

--- app/services/page.jsx
+++ app/services/page.jsx
@@ -1,7 +1,62 @@
+"use client";
+import { motion } from "framer-motion";
+import Link from "next/link";
+import { BsArrowDownRight } from "react-icons/bs";
+
+
+const services = [
+  {
+    num: "01",
+    title: "Frontend Development",
+    description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum, eum",
+    href: ""
+  },
+  {
+    num: "02",
+    title: "WordPress Development",
+    description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum, eum",
+    href: ""
+  },
+  {
+    num: "03",
+    title: "Shopify Developments",
+    description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum, eum",
+    href: ""
+  },
+  {
+    num: "04",
+    title: "E-commerce Development",
+    description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum, eum",
+    href: ""
+  },
+]
+
 
 const Services = () => {
   return (
-    <div>Services</div>
+    <section className="min-h-[80vh] flex flex-col justify-center py-12">
+      <div className="container mx-auto">
+        <motion.div initial={{opacity: 0}} animate={{opacity: 1, transition: {delay: 2.4, duration: 0.4, ease: "easeIn"}}} className="grid grid-cols-1 md:grid-cols-2 gap-[60px]">
+          {
+            services.map((service, index) => {
+              return (
+                <div key={index} className="flex-1 flex flex-col gap-6 justify-center group">
+                   <div className="flex justify-between w-full items-center">
+                    <div className="text-5xl font-extrabold text-outline text-transparent group-hover:text-outline-hover cursor-pointer transition-all duration-500">{service.num}</div>
+                    <Link href={service.href} className="w-[50px] h-[50px] bg-white group-hover:bg-accent rounded-full transition-all duration-500 flex justify-center items-center hover:-rotate-45">
+                    <BsArrowDownRight className="text-2xl text-primary" />
+                    </Link>
+                   </div>
+                   <h2 className="h2 text-white group-hover:text-accent">{service.title}</h2>
+                   <p className="text-white/60 text-[16px]">{service.description}</p>
+                   <div className="border-b border-white/20 w-full"></div>
+                </div>
+              )
+            })
+          }
+        </motion.div>
+      </div>
+    </section>
   )
 }
 
