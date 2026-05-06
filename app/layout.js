72ed7e431ae28d5d4dd557715861961bfc76a368
Hasan Wazid <webexguruhasan95@gmail.com>
2026-05-06 12:36:30 +0600

    Add SiteAgent widget integration with token

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,7 @@
 import Header from "@/components/Header";
 import PageTransition from "@/components/PageTransition";
 import StairTransition from "@/components/StairTransition";
+import Script from "next/script";
 import { JetBrains_Mono } from "next/font/google";
 import "./globals.css";
 
@@ -17,6 +18,17 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={jetBrainsMono.variable}>
+        <Script id="siteagent-config" strategy="beforeInteractive">
+          {`window.SiteAgent = {
+            token: "sa_pub_v1.eyJzaXRlVXJsIjoiaHR0cHM6Ly9oYXNhbi13YXppZC1wb3J0Zm9saW8udmVyY2VsLmFwcCIsImRvbWFpbiI6Imhhc2FuLXdhemlkLXBvcnRmb2xpby52ZXJjZWwuYXBwIiwiaXNzdWVkQXQiOjE3NzgwNDkyMzY1ODUsImV4cGlyZXNBdCI6MTgwOTU4NTIzNjU4NSwicGxhbiI6ImZyZWUifQ.rkIarryMw58H2hzLK5ZT3f42NFVKshhIEUoV9tg5lBs",
+            position: "bottom-right",
+            theme: "auto"
+          }`}
+        </Script>
+        <Script
+          src="https://cdn.jsdelivr.net/gh/Webexguru-Hasan/siteagent-widget@v1.0.3/siteagent.js"
+          strategy="lazyOnload"
+        />
         <StairTransition />
         <Header />
         <PageTransition>{children}</PageTransition>
